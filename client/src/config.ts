3fc3081eccd4e67410ec32ecac798064308257d1
Mike Jackson <jackson-mike@sbcglobal.net>
2020-05-08 01:22:32 -0400

    trial build

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,11 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'ihmbdutrq9'
+const apiId = 'dqj4fs190knp'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'junglebadger.auth0.com',            // Auth0 domain
-  clientId: 'uERpqzbMJXxnlGddZZfTY9sPhNJJtcRa',          // Auth0 client id
+  domain: 'dev-0hz4le1t.auth0.com',            // Auth0 domain
+  clientId: 'm3sGL6d7ZiR3SqyMvYybMs5ygsx6hlKJ',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
 
